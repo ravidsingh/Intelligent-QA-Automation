63333294d09e7ab2ed2aa69d8627e857655e2fdd
ravidsingh <ravidsingh@USHYDRAVIDSING2.us.deloitte.com>
2023-06-11 16:07:20 +0530

    Added qtest integration

--- src/main/java/com/example/delivery/service/RepoPushEventsProcessor.java
+++ src/main/java/com/example/delivery/service/RepoPushEventsProcessor.java
@@ -2,9 +2,12 @@ package com.example.delivery.service;
 
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -21,7 +24,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.delivery.model.QtestRequirement;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
@@ -41,10 +46,19 @@ public class RepoPushEventsProcessor {
 	
 	
 	@Value("${test-tool-url}")
-	private String url;
+	private String zephyreUrl;
+	
+	@Value("${qtest-url}")
+	private String qtestUrl;
 	
 	@Value("${jenkins-url}")
 	private String jenkinsUrl;
+	
+	@Value("${use-qtest}")
+	private boolean useQtest;
+	
+	@Value("${projectId}")
+	private String projectId;
 
 	private static final String HEAD_COMMIT = "head_commit";
 	private static final String REF = "ref";
@@ -72,15 +86,8 @@ public class RepoPushEventsProcessor {
 				if(matcher.find()) {
 				var jiraId = matcher.group();
 				System.out.println("Changes merged to main branch for Jira Id: " + jiraId);
-				HttpHeaders header = new HttpHeaders();
-				header.add("Authorization",
-						"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb250ZXh0Ijp7ImJhc2VVcmwiOiJodHRwczovL2lxZXBvYy5hdGxhc3NpYW4ubmV0IiwidXNlciI6eyJhY2NvdW50SWQiOiI3MTIwMjA6YmQ2NmE1MjMtZDMxYi00OWQwLWExNzktMjU1MTEwNTRlODZhIn19LCJpc3MiOiJjb20ua2Fub2FoLnRlc3QtbWFuYWdlciIsInN1YiI6Ijk5NWQ0ZGRhLTJmNTktMzE3Ny1hMmYzLTliYzIxNWI2OGZlMSIsImV4cCI6MTcxMjgyOTk0NiwiaWF0IjoxNjgxMjkzOTQ2fQ.ZbORcKZOBdYsFFdsFES4ABMS-KAHOFQGEWBJOXMLXLM");
-				response = restTemplate.exchange(url, HttpMethod.GET,
-						new HttpEntity<>(header), String.class, getParam(jiraId));
-				List<Object> zephyrResponse = parser.parseList(response.getBody());
-				var testcase = getTestCaseInfo(objectMapper, zephyrResponse);
-				var testCaseId = testcase.stream().map(val -> parser.parseMap(val).get("key")).collect(Collectors.toList());
-				System.out.println("Excecuting Test cases:" + testCaseId+" in user story "+jiraId);
+				List<String> testCaseId = useQtest ? fetchQtestTestCases(parser, jiraId, objectMapper) : fetchZephyrTestCases(parser, jiraId, objectMapper);
+				System.out.println("Excecuting Test cases:" + testCaseId+" for user story "+jiraId);
 				Process process;
 				process = Runtime.getRuntime().exec("curl -I "+jenkinsUrl);
 				InputStream stream = process.getInputStream();
@@ -98,6 +105,30 @@ public class RepoPushEventsProcessor {
 		}
 		return ResponseEntity.ok("Trigger Successful : " + result);
 	}
+
+	private List<String> fetchZephyrTestCases(JsonParser parser, String jiraId, ObjectMapper objectMapper) throws JsonProcessingException {
+		ResponseEntity<String> response;
+		HttpHeaders header = new HttpHeaders();
+		header.add("Authorization",
+				"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb250ZXh0Ijp7ImJhc2VVcmwiOiJodHRwczovL2lxZXBvYy5hdGxhc3NpYW4ubmV0IiwidXNlciI6eyJhY2NvdW50SWQiOiI3MTIwMjA6YmQ2NmE1MjMtZDMxYi00OWQwLWExNzktMjU1MTEwNTRlODZhIn19LCJpc3MiOiJjb20ua2Fub2FoLnRlc3QtbWFuYWdlciIsInN1YiI6Ijk5NWQ0ZGRhLTJmNTktMzE3Ny1hMmYzLTliYzIxNWI2OGZlMSIsImV4cCI6MTcxMjgyOTk0NiwiaWF0IjoxNjgxMjkzOTQ2fQ.ZbORcKZOBdYsFFdsFES4ABMS-KAHOFQGEWBJOXMLXLM");
+		response = restTemplate.exchange(zephyreUrl, HttpMethod.GET,
+				new HttpEntity<>(header), String.class, getZephyreParam(jiraId));
+		List<Object> zephyrResponse = parser.parseList(response.getBody());
+		var testcase = getTestCaseInfo(objectMapper, zephyrResponse);
+		var testCaseId = testcase.stream().map(val -> parser.parseMap(val).get("key").toString()).collect(Collectors.toList());
+		return testCaseId;
+	}
+	
+	private List<String> fetchQtestTestCases(JsonParser parser, String jiraId, ObjectMapper objectMapper) throws JsonProcessingException {
+		ResponseEntity<String> response;
+		HttpHeaders header = new HttpHeaders();
+		header.add("Authorization",
+				"Bearer acbd5ab9-23c4-431f-b9e4-1dc7e63edc2f");
+		response = restTemplate.exchange(qtestUrl, HttpMethod.GET,
+				new HttpEntity<>(header), String.class, getqtestParam(projectId));
+		var qtestResponse = parser.parseList(response.getBody());
+		return getTestCaseInfo(objectMapper, qtestResponse, parser, jiraId);
+	}
 	
 	private List<String> getTestCaseInfo(ObjectMapper objectMapper, List<Object> zephyrResponse)
 			throws JsonProcessingException {
@@ -112,10 +143,38 @@ public class RepoPushEventsProcessor {
 		}).collect(Collectors.toList());
 		return testcase;
 	}
+	
+	
+	private List<String> getTestCaseInfo(ObjectMapper objectMapper, List<Object> qTestResponse, JsonParser parser, String issueId)
+			throws JsonProcessingException {
+		
+		var userStories = qTestResponse.stream()
+				.map(val1 ->objectMapper.convertValue(val1, Map.class))
+				.map(key->key.get("requirements")).collect(Collectors.toList());
+		
+		List<QtestRequirement> reqList = new ArrayList<>();
+		
+		userStories.stream().forEach(us ->{
+			var list = objectMapper.convertValue(us, new TypeReference<List<Object>>() {});
+			 list.stream().forEach(li -> {
+				 reqList.add(objectMapper.convertValue(li, QtestRequirement.class));
+			 });
+		});
 
-	private Map<String, String> getParam(String value) {
+			var testCases = reqList.stream().filter(li -> li.getName().contains(issueId) && Objects.nonNull(li.getLinkedTestcases()))
+					.map(arg -> arg.getTestcases()).collect(Collectors.toList());
+		return testCases.stream().flatMap(list -> Arrays.asList(list.toString().split(", ")).stream()).collect(Collectors.toList());
+	}
+
+	private Map<String, String> getZephyreParam(String value) {
 		Map<String, String> uriVariables = new HashMap<>();
 		uriVariables.put("jiraId", value);
+		return uriVariables;
+	}
+	
+	private Map<String, String> getqtestParam(String value) {
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("projectId", value);
 		return uriVariables;
 	}
 }
